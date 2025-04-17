5d165cc132eeb20f37c4a891b1eb36ed55720464
varunvinod30 <varunvinod30@gmail.com>
2025-04-17 19:41:45 +0530

    fixed

--- routes/scorecard.js
+++ routes/scorecard.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const multer = require("multer");
-const { uploadPDF, getAllMatches } = require("../utils/pdfParser");
+const { uploadPDF, getAllMatches,validateStumpsReport } = require("../utils/pdfParser");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() }); // Store in memory only
 
 router.post("/", upload.single("pdf"), uploadPDF);   // Upload + AI parse
 router.get("/", getAllMatches);                      // Fetch all parsed matches
-router.post('/validateStumpsReport', upload.single('pdf'), scorecardController.validateStumpsReport);
+router.post('/validateStumpsReport', upload.single('pdf'), validateStumpsReport);
 
 module.exports = router;
