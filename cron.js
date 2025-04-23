da2db30778a0bcac656f8710894d503dbdf30867
varunvinod30 <varunvinod30@gmail.com>
2025-04-23 21:36:00 +0530

    cron updated

--- cron.js
+++ cron.js
@@ -5,11 +5,11 @@ const backendUrl = 'https://wccbackendoffl.onrender.com/api/teams';
 const job = new cron.CronJob('*/14 * * * *', function(){
     const currentHour = new Date().getHours();
     const currentDay = new Date().getDay();
-    if (currentDay === 5 && currentHour >= 1 && currentHour < 4) {
-        console.log('Cron job skipped: Scheduled maintenance window (1 AM - 4 AM)');
+    if (currentDay === 6 && currentHour >= 1 && currentHour < 5) {
+        console.log('Cron job skipped: Scheduled maintenance window (1 AM - 5 AM)');
         return;}
-    if ((currentDay === 2 || currentDay === 3 || currentDay === 4) && (currentHour >= 11 && currentHour < 13)) {
-        console.log('Cron job skipped: Scheduled downtime on Tue-Thu (11 AM - 1 PM)');
+    if ((currentDay === 2 || currentDay === 3 || currentDay === 4) && (currentHour >= 11 && currentHour < 14)) {
+        console.log('Cron job skipped: Scheduled downtime on Tue-Thu (11 AM - 2 PM)');
         return;
     }
     console.log('Restarting server');
