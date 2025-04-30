2da7d75fa32efb9732aa47f4391a6f8404dce5b3
varunvinod30 <varunvinod30@gmail.com>
2025-04-30 18:09:24 +0530

    works as intended

--- routes/scorecard.js
+++ routes/scorecard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { uploadPDF, getAllMatches,validateStumpsReport,playerstat,playerstatadd,validatePlayerNamesFromPDF,extractPlayerNames,validatePlayerNames,updatePlayerNames } = require("../utils/pdfParser");
+const { uploadPDF, getAllMatches,validateStumpsReport,playerstat,playerstatadd,validatePlayerNamesFromPDF,extractPlayerNames,validatePlayerNames,updatePlayerNames,allscorecard } = require("../utils/pdfParser");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() }); // Store in memory only
@@ -14,5 +14,6 @@ router.post("/validate-player-names", upload.single("pdf"), validatePlayerNamesF
 router.post("/extractPlayerNames", upload.single("pdf"), extractPlayerNames);     
 router.post("/validatePlayerNames", validatePlayerNames);                         
 router.post("/updatePlayerNames", updatePlayerNames);
+router.get("/allscorecard", allscorecard);
 
 module.exports = router;
